439621fdf3f7ded085fe10238ff5265b1d881301
valentim.koniarski <valentim.koniarski@hotmail.com>
2023-10-22 00:42:09 -0300

    ajuste css global

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <v-app :theme="theme">
-
-
-
-    <Header />
-
-
+    <Header/>
     <v-main>
       <v-container>
-        <router-view />
+        <router-view/>
       </v-container>
     </v-main>
   </v-app>
@@ -25,24 +20,5 @@ const store = useStore();
 
 const theme = computed(() => store.getters['getTheme']);
 
-
-
 </script>
 
-<style>
-::-webkit-scrollbar {
-  width: .60rem;
-  background-color: #B388FF;
-}
-
-::-webkit-scrollbar-thumb {
-  background-color: #D1C4E9;
-  border-radius: .5rem;
-}
-
-::-webkit-scrollbar-thumb:hover {
-  background-color: #EDE7F6;
-}
-
-
-</style>
