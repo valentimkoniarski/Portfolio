cf0e7385a596d3872f734634bd40ff9920f728ea
valentimkoniarski <valentim.koniarski@hotmail.com>
2023-11-28 19:22:42 -0300

    ajuste imagem de perfil, e criado regras no eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,8 @@ module.exports = {
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
+    quotes: ['warn', 'single', { avoidEscape: true }], // Define aspas simples
+    'indent': ['error', 2], // Define o número de espaços para indentação
+    'object-curly-spacing': ['error', 'always'], // Adiciona espaços em chaves
   },
 }
