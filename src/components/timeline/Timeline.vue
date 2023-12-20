4dd455f5921dcfbe65e5775d8da21dc4ff250d02
valentim koniarski <valentim.koniarski@hotmail.com>
2023-12-20 19:09:28 -0300

    ajustado timeline e adicionado novas experiencias

--- src/components/timeline/Timeline.vue
+++ src/components/timeline/Timeline.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="timeline mt-10">
     <h5 class="text-h5 text-uppercase text-center text-deep-purple-accent-1">Education & work</h5>
-    <v-timeline class="mt-5">
-      <v-timeline-item v-for="timeline in timelines" :key="timeline.id" :dot-color="timeline.color"
-                       :icon="timeline.icon" size="large">
-        <template v-slot:opposite>
-          <div :class="`pt-1 headline font-weight-bold text-button text-${timeline.color}`"
-               v-text="timeline.period"></div>
-        </template>
+    <v-timeline side="end" class="mt-5">
+      <v-timeline-item
+        v-for="timeline in timelines" :key="timeline.id"
+        :dot-color="timeline.color" :icon="timeline.icon" size="large"
+      >
         <div>
-          <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${timeline.color}`">{{ timeline.title }}</h2>
+          <h2
+            :class="`mt-n1 headline font-weight-light mb-4 text-${timeline.color}`"
+          >
+          {{ timeline.title }}
+        </h2>
+          <div
+            :class="`pt-1 headline font-weight-bold text-button text-${timeline.color}`"
+               v-text="timeline.period"
+          ></div>
           <h5 :class="`text-button text-${timeline.color}`">{{ timeline.position }}</h5>
           <span class="text-subtitle-2" v-html="timeline.description"></span>
 
           <div v-if="timeline.techlist" class="mt-5">
             <h5 class="text-button text-amber darken-1">Techlist</h5>
             <div class="d-flex flex-wrap">
-              <v-chip class="mr-2 mb-2" v-for="(tech, i) in timeline.techlist" :key="i" text-color="white" small>
+              <v-chip
+                class="mr-2 mb-2" v-for="(tech, i) in timeline.techlist" :key="i"
+                text-color="white" small
+              >
                 <span class="text-subtitle-2">{{ tech }}</span>
               </v-chip>
             </div>
@@ -60,12 +69,23 @@ const timelines = reactive([
     color: 'blue-darken-3',
     title: 'Betha Sistemas',
     position: 'Developer',
-    period: 'APR 2022 — NOWADAYS',
+    period: 'APR 2022 — DEZ 2023',
     description: '' +
         '<p> Working at the inspection management system, with microservices based in Java, using Spring Boot with ' +
         'messaging for services communication, JPA with Hibernate and Querydsl and unit testing with JUnit; </p><br>' +
         '<p> Frontend development component based with Angular.JS, VueJS and Javascript ECMA 6; </p>',
     techlist: ['Java', 'Spring boot', 'Hibernate', 'Vue.js', 'AngularJS', 'Mockito', 'JUnit', 'PostgreSQL']
+  },
+  {
+    id: 2,
+    icon: 'mdi-briefcase-variant',
+    color: 'green-darken-3',
+    title: 'INMETA Sistemas',
+    position: 'Developer',
+    period: 'DEZ 2023 — NOWADAYS',
+    description: '' +
+        '<p></p>',
+    techlist: ['NodeJS', 'MongoDB', 'Vue.js', 'Typescript', 'PostgreSQL']
   }
 ])
 </script>
