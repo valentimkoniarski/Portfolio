d02b2d85b5d82f2c4f5ded2962096330815da66c
valentim koniarski <valentim.koniarski@hotmail.com>
2023-12-20 19:20:52 -0300

    ajustado imagem de perfil

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,12 +2,14 @@
   <v-container>
     <div class="profile">
       <div class="profile__image">
-        <v-avatar :size="smAndDown ? 200 : 300">
-          <img src="../assets/images/valentim_photo.jpeg" alt="avatar" width="200" style="border-radius: 50%;">
+        <v-avatar :size="220">
+          <img src="../assets/images/valentim_photo.jpeg" alt="avatar"
+            width="220"
+          >
         </v-avatar>
       </div>
       <div class="profile__text">
-        <h1 class="text-uppercase font-weight-regular text-deep-purple-accent-1" :class="xs ? 'text-h3' : 'text-h2'">
+        <h1 class="text-uppercase font-weight-regular text-deep-purple-accent-1 text-h3">
           Valentim Koniarski</h1>
         <h2 class="text-h5 text-uppercase font-weight-light mt-5 text-deep-purple-lighten-1">Full-stack web
           developer</h2>
@@ -19,7 +21,6 @@
           <personal-info/>
         </div>
 
-
         <div class="block__element block__element-2">
           <projects/>
           <timeline/>
@@ -29,15 +30,12 @@
   </v-container>
 </template>
 <script lang="ts" setup>
-import {defineComponent} from 'vue'
 import PersonalInfo from '@/components/personalInfo/PersonalInfo.vue'
 import Timeline from '@/components/timeline/Timeline.vue'
 import Projects from '@/components/projects/Projects.vue'
+import { useDisplay } from 'vuetify'
 
-import {useDisplay} from "vuetify";
-
-const {xs, sm, smAndDown, md, mdAndDown} = useDisplay()
-
+const { xs, mdAndDown } = useDisplay()
 
 </script>
 
